fd23dfb99e9b82394078a28ce8b66e32b777a00e
xuae <55976226+xuae@users.noreply.github.com>
2019-10-21 21:14:50 +0800

    新建Vue项目，完成Vue项目搭建文章的编写。

---
+++ vue/src/router/index.ts
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: '/component/',
+    name: 'component',
+    component: () => import("../views/blog/component/Index.vue"),
+    children: [
+      {
+        path: 'function',
+        component: () => import("../views/blog/component/function/Parent.vue")
+      }
+    ]
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;

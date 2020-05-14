79a12f9a65e15a82f02865a3c36510bfad00387f
xuae <xuae.git@foxmail.com>
2020-05-14 18:20:21 +0800

    :fire: docs: 删除构建后的文件夹

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
    * 如果你打算发布到 https://<USERNAME>.github.io/，则可以省略这一步，因为 base 默认即是 "/"。
    * 如果你打算发布到 https://<USERNAME>.github.io/<REPO>/（也就是说你的仓库在 https://github.com/<USERNAME>/<REPO>），则将 base 设置为 "/<REPO>/"。
    */
-  base: '/',
+  base: '/blog/',
 
   /**
    * 指定 vuepress build 的输出目录

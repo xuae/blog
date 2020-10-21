1f1d48325ed43a85200da895c46185690210f905
xuae <xuae.git@foxmail.com>
2020-10-21 10:55:36 +0800

    :ambulance: fix: 修复评论功能无法使用

--- .vuepress/config/valine.js
+++ .vuepress/config/valine.js
@@ -3,8 +3,8 @@
  */
 
 module.exports = {
-  appId: 'nTyjdmMIGU2leFALkOaWfrJV-gzGzoHsz',
-  appKey: 'Alpnj1Y61kp9HDvunxryj9p1',
+  appId: 'udSartPs8hIQEHEh7Duv0oNg-9Nh9j0Va',
+  appKey: 'J62Rzb2WyQ2JW9j4uwvQjMUE',
   placeholder: '共同学习，共同进步', // default: Just go go
   avatar: 'wavatar', // Gravatar 头像展示方式
   enableQQ: true, // 是否启用昵称框自动获取QQ昵称和QQ头像, 默认关闭

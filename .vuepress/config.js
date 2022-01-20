40acfa330a13b9a5475ab39cde0675fcbcc46fe4
xuae <xuae.git@foxmail.com>
2022-01-20 09:46:13 +0800

    🐛 删除顶部分类导航

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -106,12 +106,12 @@ module.exports = {
      * 博客配置
      */
     blogConfig: {
-      category: {
-        location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: '分类', // 默认文案 “分类”
-      },
+      // category: {
+      //   location: 2, // 在导航栏菜单中所占的位置，默认2
+      //   text: '分类', // 默认文案 “分类”
+      // },
       tag: {
-        location: 3, // 在导航栏菜单中所占的位置，默认3
+        location: 2, // 在导航栏菜单中所占的位置，默认3
         text: '标签', // 默认文案 “标签”
       }
     },
